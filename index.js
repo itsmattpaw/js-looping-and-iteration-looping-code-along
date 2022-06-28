d0d638f411ff1b828b41ae268b0a1f6778f8b95a
itsmattpaw <itsmattpaw@gmail.com>
2022-06-28 13:37:04 -0400

    commit tree 82309b61edc8dad9cb1a483850db76ecf83efa8b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(a, b) {
+    let messages = [];
+    for (let i = 0; i < a.length; i++){
+        messages.push(`Thank you, ${a[i]}, for the wonderful ${b} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
